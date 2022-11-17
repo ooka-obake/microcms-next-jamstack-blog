6164df3a424d97b03206123ca7b0790b7e7f2492
ooka <>
2022-11-17 15:42:07 +0900

    client.tsx 修正

--- libs/client.tsx
+++ libs/client.tsx
@@ -2,6 +2,6 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-  serviceDomain: 'appxohjyq0',
-  apiKey: process.env.API_KEY,
+  serviceDomain: process.env.SERVICE_DOMAIN || "",
+  apiKey: process.env.API_KEY || "",
 });
